9d2f3fbba3b8b7418445d19c0e9dc1224d7cfec4
torutilla <christiantorres0418@gmail.com>
2025-11-22 17:13:38 +0800

    added stroke animation

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -10,22 +10,37 @@ function IconButton({ text, onClick, icon: Icon }: IconButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="relative
-      p-2 border border-(--accent-color) 
-      cursor-pointer hover:border-(--accent-color)
+      className="group relative
+      p-2 cursor-pointer
       text-(--accent-color) 
       hover:text-(--hover-text-color) 
-      transition-all duration-300 ease-in-out
+      transition-all duration-400 ease-in-out
       before:absolute 
       before:inset-0 before:scale-x-0
       before:bg-(--accent-color) 
       before:origin-left
-      before:duration-300 before:transition-all
+      before:duration-400 before:transition-all
       hover:before:scale-x-100
       before:ease-in-out before:-z-10"
     >
       {Icon && <Icon fontSize="inherit" className="" />}
       {text}
+      <svg className="absolute inset-0 w-full h-full" fill="none">
+        <rect
+          x="0"
+          y="0"
+          width="100%"
+          height="100%"
+          rx="0"
+          stroke-width="2.5"
+          pathLength="100"
+          stroke-dasharray="100"
+          stroke-dashoffset="0"
+          className="stroke-(--accent-color) 
+          transition-[stroke-dashoffset] duration-700 
+          group-hover:[stroke-dashoffset:100]"
+        />
+      </svg>
     </button>
   );
 }
