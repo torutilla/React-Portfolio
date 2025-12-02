47d40451bc8557223f2f75aa29de2944940642fe
torutilla <christiantorres0418@gmail.com>
2025-12-02 23:48:15 +0800

    changed shimmertext to herotitle

---
+++ src/components/common/texts/HeroTitle.tsx
@@ -0,0 +1,25 @@
+type ShimmerTextProps = {
+  text: string;
+};
+
+function HeroTitle({ text }: ShimmerTextProps) {
+  return (
+    <div className="overflow-hidden">
+      <h1
+        data-text={text}
+        className={`
+          ml-4 mr-4 relative font-extrabold text-3xl sm:text-4xl md:text-5xl lg:text-6xl
+        font-title
+        after:bg-linear-to-r after:from-transparent 
+        after:via-white/50 after:text-transparent
+        after:to-transparent after:animate-shimmer after:bg-size-[200%_200%] 
+        after:absolute after:bg-clip-text after:inset-0
+        after:content-[attr(data-text)] after:z-999 cursor-text after:cursor-text`}
+      >
+        {text}
+      </h1>
+    </div>
+  );
+}
+
+export default HeroTitle;
