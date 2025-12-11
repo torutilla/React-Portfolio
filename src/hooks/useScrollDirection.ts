78c76549273b4434af081498f4701f82a990041e
torutilla <christiantorres0418@gmail.com>
2025-12-11 18:01:10 +0800

    added default value on direction

--- src/hooks/useScrollDirection.ts
+++ src/hooks/useScrollDirection.ts
@@ -1,16 +1,15 @@
 import { useEffect, useRef, useState } from "react";
 
 export function useScrollDirection(threshold: number = 10){
-    const [direction, setDirection] = useState<String>("");
+    const [direction, setDirection] = useState<String>("up");
     const prevScrollRef = useRef(0);
     useEffect(()=>{
         const handleScroll = ()=>{
             const current = window.scrollY;
             const prev = prevScrollRef.current;
             const diff = current - prev;
-
             if(Math.abs(diff) > threshold){
-                setDirection(diff > 0? "down": "up");
+                setDirection(diff > 0? "down" : "up")
                 prevScrollRef.current = current;
             }
         }
