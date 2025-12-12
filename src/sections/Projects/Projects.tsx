f4e20017aa54cec0edb3ee8943669543ed41d742
torutilla <christiantorres0418@gmail.com>
2025-12-13 02:10:31 +0800

    try scroll trigger on home page

---
+++ src/sections/Projects/Projects.tsx
@@ -0,0 +1,12 @@
+function Projects() {
+  return (
+    <div
+      id="Projects"
+      className="w-full h-dvh bg-gray-400 flex flex-col items-center pt-5"
+    >
+      <p className="font-title text-text text-3xl">Projects</p>
+    </div>
+  );
+}
+
+export default Projects;
