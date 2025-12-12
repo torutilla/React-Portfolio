68e726d43e97e5fc01d21bd946692b12027aa039
torutilla <christiantorres0418@gmail.com>
2025-12-12 15:26:20 +0800

    added current scroll on useScroll hook

--- src/hooks/useScrollDirection.ts
+++ src/hooks/useScrollDirection.ts
@@ -1,15 +1,32 @@
 import { useEffect, useRef, useState } from "react";
-
+type ScrollDirectionType = {
+    direction: "up" | "down";
+    currentScroll: number;
+}
 export function useScrollDirection(threshold: number = 10){
-    const [direction, setDirection] = useState<String>("up");
+    const [direction, setDirection] = useState<ScrollDirectionType>({direction: "up", currentScroll: 0});
     const prevScrollRef = useRef(0);
+    const lastDirection = useRef<"up" | "down">("up");
     useEffect(()=>{
         const handleScroll = ()=>{
             const current = window.scrollY;
             const prev = prevScrollRef.current;
             const diff = current - prev;
+
+
+            console.log(current);
             if(Math.abs(diff) > threshold){
-                setDirection(diff > 0? "down" : "up")
+                const newDirection = diff > 0 ? "down": "up";
+                
+                if(newDirection !== lastDirection.current){
+                    lastDirection.current = newDirection;
+                    setDirection({
+                        direction: newDirection,
+                        currentScroll: current,
+                    });
+                }else{
+                    setDirection(prev=> ({...prev, currentScroll:current}))
+                }
                 prevScrollRef.current = current;
             }
         }
