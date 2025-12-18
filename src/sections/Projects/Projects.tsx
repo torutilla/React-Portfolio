4ba9a34a15d295ff3848b951165c893ac19e4f2c
torutilla <christiantorres0418@gmail.com>
2025-12-19 03:05:51 +0800

    added page animation on projects section

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -1,10 +1,48 @@
+import gsap from "gsap";
+import { useLayoutEffect, useRef } from "react";
+
 function Projects() {
+  const ref = useRef<HTMLDivElement>(null);
+  useLayoutEffect(() => {
+    gsap.fromTo(
+      "#Projects",
+      { borderRadius: "300px" },
+      {
+        borderRadius: "0px",
+        scrollTrigger: {
+          trigger: "#Projects",
+          start: "top bottom",
+          end: "top center",
+          scrub: true,
+          markers: true,
+        },
+      }
+    );
+
+    const inner = document.getElementById("projects-inner");
+    gsap.to(inner, {
+      y: () => -(inner!.scrollHeight - window.innerHeight),
+      ease: "none",
+      scrollTrigger: {
+        trigger: "#Projects",
+        start: "top top",
+        end: () => "+=" + (inner!.scrollHeight - window.innerHeight),
+        scrub: true,
+      },
+    });
+  }, []);
+
   return (
     <div
       id="Projects"
-      className="w-full h-dvh bg-gray-400 flex flex-col items-center pt-5"
+      ref={ref}
+      className="relative w-full h-dvh bg-gray-400 flex flex-col items-center pt-5 z-20 overflow-hidden"
     >
-      <p className="font-title text-text text-3xl">Projects</p>
+      <p className="font-title text-text text-title">Projects</p>
+      <div
+        id="projects-inner"
+        className="relative will-change-transform min-h-[200dvh]"
+      ></div>
     </div>
   );
 }
