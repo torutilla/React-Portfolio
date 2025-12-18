3f1b6f54d133437ecf37f71c6285e7d38261981a
torutilla <christiantorres0418@gmail.com>
2025-12-19 03:08:47 +0800

    fillbutton animation

--- src/components/common/buttons/FillButton.tsx
+++ src/components/common/buttons/FillButton.tsx
@@ -2,22 +2,27 @@ import gsap from "gsap";
 import { useRef } from "react";
 import useRippleAnimation from "../../../hooks/useRippleAnimation.ts";
 type FillButtonProps = {
+  ref?: React.RefObject<HTMLButtonElement | null>;
   children?: React.ReactNode;
   variant?: "default" | "icon";
 };
-function FillButton({ children, variant }: FillButtonProps) {
+function FillButton({ children, variant, ref }: FillButtonProps) {
   const variantStyle =
     variant == "icon" ? "rounded-full" : "rounded-2xl pl-4 pr-4";
-  const btnRef = useRef<HTMLButtonElement>(null);
+  const btnRef = ref ? ref : useRef<HTMLButtonElement>(null);
   const ripple = useRippleAnimation(btnRef);
   const onMouseEnter = () => {
-    gsap.to(btnRef.current, { y: -2, duration: 0.2, ease: "bounce.in" });
+    // gsap.to(btnRef.current, { y: -2, duration: 0.2, ease: "bounce.in" });
   };
   const onMouseLeave = () => {
-    gsap.to(btnRef.current, { y: 0, duration: 0.2 });
+    // gsap.to(btnRef.current, { y: 0, duration: 0.2 });
   };
   const onClick = (e: React.MouseEvent) => {
-    gsap.to(btnRef.current, { y: 0, duration: 0.2 });
+    gsap.fromTo(
+      btnRef.current,
+      { y: -1, ease: "circ.in" },
+      { y: 0, duration: 0.3 }
+    );
     ripple(e);
   };
   return (
