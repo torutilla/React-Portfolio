2b21acadc9eef75c3d9f8d266ac6bdd7f5af7f97
torutilla <christiantorres0418@gmail.com>
2025-12-19 03:07:55 +0800

    moved socials

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,8 +1,6 @@
 import WipeAnimator from "../../components/animator/WipeAnimator.tsx";
 import OutlineButton from "../../components/common/buttons/OutlineButton.tsx";
-import FillButton from "../../components/common/buttons/FillButton.tsx";
 import HeroTitle from "../../components/common/texts/HeroTitle.tsx";
-import { Email, GitHub, LinkedIn } from "@mui/icons-material";
 
 function Hero() {
   return (
@@ -22,20 +20,6 @@ function Hero() {
       </div>
       <div className="call-to-action flex gap-2 justify-center">
         <OutlineButton>View Resume</OutlineButton>
-      </div>
-      <div
-        id="socials-container"
-        className="absolute right-6 bottom-10  h-auto flex gap-sm"
-      >
-        <FillButton variant="icon">
-          <GitHub />
-        </FillButton>
-        <FillButton variant="icon">
-          <Email />
-        </FillButton>
-        <FillButton variant="icon">
-          <LinkedIn />
-        </FillButton>
       </div>
     </div>
   );
