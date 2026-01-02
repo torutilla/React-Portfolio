afeb9a73e3c0e3e221142a0c77f43b939b14b690
torutilla <christiantorres0418@gmail.com>
2026-01-03 02:10:25 +0800

    moved types in theme.ts

--- src/components/common/texts/SlideAnimationText.tsx
+++ src/components/common/texts/SlideAnimationText.tsx
@@ -1,41 +1,12 @@
 import { useRef } from "react";
 import useSlideAnimation from "../../../hooks/useSlideAnimation.ts";
-import type {
-  ColorOptions,
-  FontVariant,
-  TextSize,
-} from "../../../types/theme.ts";
-type TextStyle = {
-  variant: FontVariant;
-  size: TextSize;
-  color?: ColorOptions;
-};
+import { getTextClasses, type TextStyle } from "../../../types/theme.ts";
+
 type SlideTextProps = {
   style: TextStyle;
   trigger: string;
   children?: React.ReactNode;
   triggerOverrides?: ScrollTrigger.Vars;
-};
-
-const variantMap: Record<FontVariant, string> = {
-  title: "font-title",
-  subtitle: "font-subtitle",
-  body: "font-body",
-};
-
-const sizeMap: Record<TextSize, string> = {
-  sm: "text-sm",
-  md: "text-lg",
-  lg: "text-xl",
-  xl: "text-2xl",
-  "2xl": "text-title",
-};
-
-const colorMap: Record<ColorOptions, string> = {
-  text: "text-text",
-  dark: "text-black/75",
-  accent: "text-accent",
-  primary: "text-primary",
 };
 
 function SlideAnimationText({
@@ -56,11 +27,6 @@ function SlideAnimationText({
       </div>
     </div>
   );
-}
-
-function getTextClasses(style: TextStyle) {
-  const { variant, size, color } = style;
-  return `${variantMap[variant]} ${sizeMap[size]} ${colorMap[color ?? "text"]}`;
 }
 
 export default SlideAnimationText;
--- src/types/theme.ts
+++ src/types/theme.ts
@@ -1,3 +1,34 @@
 export type ColorOptions = "text" | "accent" | "primary" | "dark";
 export type FontVariant = "title" | "subtitle" | "body";
 export type TextSize = "sm" | "md" | "lg" | "xl" | "2xl";
+export const variantMap: Record<FontVariant, string> = {
+    title: "font-title",
+    subtitle: "font-subtitle",
+    body: "font-body",
+  };
+  
+export  const sizeMap: Record<TextSize, string> = {
+    sm: "text-sm",
+    md: "text-lg",
+    lg: "text-xl",
+    xl: "text-2xl",
+    "2xl": "text-title",
+  };
+  
+export const colorMap: Record<ColorOptions, string> = {
+    text: "text-text",
+    dark: "text-black/75",
+    accent: "text-accent",
+    primary: "text-primary",
+  };
+
+export type TextStyle = {
+    variant?: FontVariant;
+    size?: TextSize;
+    color?: ColorOptions;
+};
+
+export function getTextClasses(style: TextStyle) {
+    const { variant, size, color } = style;
+    return `${variantMap[variant ?? "title"]} ${sizeMap[size ?? "2xl"]} ${colorMap[color ?? "text"]}`;
+  }
